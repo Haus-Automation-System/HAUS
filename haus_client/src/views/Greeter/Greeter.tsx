b955887faa9264f8a814ccd9b6210a421386ce59
iTecAI <matteovh@gmail.com>
2023-12-29 16:04:03 -0500

    wip greeter & icons

--- haus_client/src/views/Greeter/Greeter.tsx
+++ haus_client/src/views/Greeter/Greeter.tsx
@@ -1,12 +1,21 @@
-import { Paper } from "@mantine/core";
+import { Paper, Stack } from "@mantine/core";
+import { useTranslation } from "react-i18next";
+import AppLogo from "../../assets/haus_logo.png";
+import { useColorScheme } from "@mantine/hooks";
 
 export function Greeter() {
+    const { t } = useTranslation();
+    const colorScheme = useColorScheme();
     return (
-        <Paper
-            className="greeter-container"
-            radius="sm"
-            p="sm"
-            shadow="sm"
-        ></Paper>
+        <Stack gap="sm" className="greeter-container">
+            <Paper className="greeter-box logo" radius="sm" p="sm" shadow="sm">
+                <Stack gap="sm" align="center" justify="center">
+                    <img src={AppLogo} className={`app-logo ${colorScheme}`} />
+                </Stack>
+            </Paper>
+            <Paper className="greeter-box form" radius="sm" p="sm" shadow="sm">
+                {t("views.greeter.login.title")}
+            </Paper>
+        </Stack>
     );
 }
