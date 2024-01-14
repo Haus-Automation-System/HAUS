b39223c1dc6a87097037334d4cd35ebf2d380936
Dax Harris <matteovh@gmail.com>
2024-01-14 13:32:53 -0500

    update URL matching

--- haus_client/src/components/PluginData/PropertyRenderer.tsx
+++ haus_client/src/components/PluginData/PropertyRenderer.tsx
@@ -15,7 +15,7 @@ import { useTranslation } from "react-i18next";
 import { startCase } from "lodash";
 
 const URL_MATCH = new RegExp(
-    /https?:\/\/(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+    /^https?:\/\/(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/
 );
 
 export function PropertyRenderer({
